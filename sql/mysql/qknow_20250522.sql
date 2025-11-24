3e67a58fcb83c69717ced5dbd2efa2ddffbb722d
guchunxiao <guchunxiao@qiantong.tech>
2025-11-24 16:10:08 +0800

    初始化脚本修改，默认打开结构化、非结构化抽取定时任务

--- sql/mysql/qknow_20250522.sql
+++ sql/mysql/qknow_20250522.sql
@@ -598,8 +598,8 @@ create table system_job (
 insert into system_job values(1, '系统默认（无参）', 'DEFAULT', 'ryTask.ryNoParams',        '0/10 * * * * ?', '3', '1', '1', '小桐', sysdate(), '', null, '');
 insert into system_job values(2, '系统默认（有参）', 'DEFAULT', 'ryTask.ryParams(\'ry\')',  '0/15 * * * * ?', '3', '1', '1', '小桐', sysdate(), '', null, '');
 insert into system_job values(3, '系统默认（多参）', 'DEFAULT', 'ryTask.ryMultipleParams(\'ry\', true, 2000L, 316.50D, 100)',  '0/20 * * * * ?', '3', '1', '1', '小桐', sysdate(), '', null, '');
-insert into system_job values (100, '结构化任务抽取', 'DEFAULT', 'extStructTaskServiceImpl.consumeQueue()', '0 0/1 * * * ?', '1', '1', '1', '小桐', sysdate(), '', NULL, '');
-insert into system_job values (101, '非结构化任务抽取', 'DEFAULT', 'extUnstructTaskServiceImpl.consumeQueue()', '0 0/5 * * * ?', '1', '1', '1', '小桐', sysdate(), '', NULL, '');
+insert into system_job values (100, '结构化任务抽取', 'DEFAULT', 'extStructTaskServiceImpl.consumeQueue()', '0 0/1 * * * ?', '1', '1', '0', '小桐', sysdate(), '', NULL, '');
+insert into system_job values (101, '非结构化任务抽取', 'DEFAULT', 'extUnstructTaskServiceImpl.consumeQueue()', '0 0/5 * * * ?', '1', '1', '0', '小桐', sysdate(), '', NULL, '');
 
 -- ----------------------------
 -- 16、定时任务调度日志表
