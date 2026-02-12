499cfa89433d8d01e7ba3960c39631192eee402e
jinwang <Jin0928>
2026-02-12 14:20:42 +0800

    浏览器标签页图标

--- qknow-ui/src/App.vue
+++ qknow-ui/src/App.vue
@@ -78,7 +78,7 @@ const createIco = () => {
   let link = document.querySelector("link[rel*='icon']") || document.createElement("link");
   link.type = "image/x-icon";
   link.rel = "shortcut icon";
-  link.href = '/sso-favicon.png';
+  link.href = '/favicon.ico';
   document.getElementsByTagName("head")[0].appendChild(link);
 };
 </script>
