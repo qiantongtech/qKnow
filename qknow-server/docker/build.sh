486cf84d518145a54af5399f543212140bd835e2
chuhongen <1565445895@qq.com>
2026-05-08 10:03:22 +0800

    调整版本号为v2.1.0、sql数据补充

--- qknow-server/docker/build.sh
+++ qknow-server/docker/build.sh
@@ -29,16 +29,16 @@
 # 更多信息请访问：https://qknow.qiantong.tech/business.html
 #!/bin/bash
 # 删除旧镜像
-docker rmi qiantong/qknow:2.0.2
+docker rmi qiantong/qknow:2.1.0
 
 # 重新构建镜像 替换成你的代理或者自行更改国内源
 docker build --no-cache \
   --build-arg http_proxy=http://192.168.0.108:10809 \
   --build-arg https_proxy=http://192.168.0.108:10809 \
-  -t qiantong/qknow:2.0.2 .
+  -t qiantong/qknow:2.1.0 .
 
 # 启动新容器
 docker run -d \
   --name qknow \
   -p 8090:8090 \
-  qiantong/qknow:2.0.2
+  qiantong/qknow:2.1.0
