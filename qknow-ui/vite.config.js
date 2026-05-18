134d435bab4a75df986bd09ad04b38b976834bd6
chuhongen <1565445895@qq.com>
2026-05-18 17:44:42 +0800

    配置调整，版本号调整为v2.2.0

--- qknow-ui/vite.config.js
+++ qknow-ui/vite.config.js
@@ -77,12 +77,12 @@ export default defineConfig(({ mode, command }) => {
             proxy: {
                 // https://cn.vitejs.dev/config/#server-proxy
                 '/dev-api': {
-                    target: 'http://localhost:8090',
+                    target: 'http://localhost:8099',
                     changeOrigin: true,
                     rewrite: (p) => p.replace(/^\/dev-api/, '')
                 },
                 '/v3/api-docs': {
-                    target: 'http://localhost:8090',
+                    target: 'http://localhost:8099',
                     changeOrigin: true,
                     rewrite: (p) => p.replace('', '')
                 },
