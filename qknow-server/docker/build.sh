8f035f2e0f8cc48e5eeed576ef79a872e10073c2
chuhongen <1565445895@qq.com>
2026-07-31 10:06:04 +0800

    feat:版本号更新为2.4.0

--- qknow-server/docker/build.sh
+++ qknow-server/docker/build.sh
@@ -15,16 +15,16 @@
 # See the LICENSE file in the project root for full license information.
 #!/bin/bash
 # 删除旧镜像
-docker rmi qiantong/qknow:2.3.0
+docker rmi qiantong/qknow:2.4.0
 
 # 重新构建镜像 替换成你的代理或者自行更改国内源
 docker build --no-cache \
   --build-arg http_proxy=http://192.168.0.108:10809 \
   --build-arg https_proxy=http://192.168.0.108:10809 \
-  -t qiantong/qknow:2.3.0 .
+  -t qiantong/qknow:2.4.0 .
 
 # 启动新容器
 docker run -d \
   --name qknow \
   -p 8090:8090 \
-  qiantong/qknow:2.3.0
+  qiantong/qknow:2.4.0
